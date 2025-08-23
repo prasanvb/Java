e7c79de12e5977d4f49ce5204c37924a516c3807
prasanv <prasan.venkatb@gmail.com>
2025-08-22 20:29:04 -0700

    first java core commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    // shorthand "psvm" to create main method
+    public static void main(String[] args) {
+        // shorthand "sout" to create print statement
+        System.out.println("Hello world, here I come Java");
+
+        int x= 10;
+        int y= x+2;
+        int z = x+y;
+        System.out.println("z="+z);
+    }
+}
